1998c69f63d6277b13c45a771c0f24fee635086f
Alexander Myshov <simplex.light@gmail.com>
2020-09-25 02:35:57 +0700

    delete old files

--- example/firstWorker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('message', (e) => {
-  console.log('frist worker: start');
-
-  const someValue = 1000;
-
-  let iterations = Math.floor(Math.random() * 1000000);
-  for (let i = 0; i < iterations; i++) {}
-  console.log('frist worker: done, total iterations is', iterations);
-});
--- example/secondWorker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('message', (e) => {
-  console.log('second worker: start');
-
-  const someValue = 1000;
-
-  let iterations = Math.floor(Math.random() * 1000000);
-  for (let i = 0; i < iterations; i++) {}
-  console.log('second worker: done, total iterations is', iterations);
-});
